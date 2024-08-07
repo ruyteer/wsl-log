66469e0893c38aa430a2966ce243b00bc4dfd2db
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2024-08-07 15:06:48 -0300

    finish grosso

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import "aos/dist/aos.css";
 import { Navbar } from "@/components/ui/navbar";
 import { AOSProvider } from "@/providers/aos";
+import Link from "next/link";
+import Image from "next/image";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -21,6 +23,17 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
         <AOSProvider />
+        <Link
+          href={
+            "https://api.whatsapp.com/send?phone=5577981140394&text=Quero%20fazer%20um%20orçamento%20hoje!"
+          }
+          className="fixed bottom-10 right-10 flex flex-col items-center"
+        >
+          <p className=" max-sm:hidden text-xs bg-white text-black p-1 rounded-md absolute right-20 w-[100px] text-center">
+            Contate no Whatsapp!
+          </p>
+          <Image src={"/zap.webp"} width={70} height={70} />
+        </Link>
       </body>
     </html>
   );
--- src/components/pages/home/footer.jsx
+++ src/components/pages/home/footer.jsx
@@ -14,40 +14,41 @@ export function Footer() {
           src={"/logo.jpeg"}
           width={200}
           height={200}
-          className="rounded-lg"
+          className="rounded-lg max-sm:w-[150px]"
         />
-        <section className="flex items-center justify-between  space-x-36 mt-20">
-          <div className="flex flex-col items-start ">
+        <section className="flex items-center max-sm:items-start max-sm:flex-col max-sm:justify-center max-sm:gap-10  justify-between md:space-x-36  lg:space-x-36 mt-20">
+          <div className="flex flex-col items-start  ">
             <h1 className="text-2xl font-semibold">Sede</h1>
 
             <p className="mt-5 text-sm">Endereço Wsl Log aqui</p>
             <p className="mt-1 text-sm">Brasília - DF</p>
           </div>
-          <div className="flex flex-col items-start ">
+          <div className="flex flex-col items-start  ">
             <h1 className="text-2xl font-semibold">Contato</h1>
 
             <Link
               className="mt-5 text-sm"
               href={"mailto:wallace.oliveira@wsllog.com.br"}
             >
-              Email - wallace.oliveira@wsllog.com.br
+              Email -{" "}
+              <span className="underline">wallace.oliveira@wsllog.com.br</span>
             </Link>
             <Link
-              className="mt-1 text-sm"
+              className="mt-1 text-sm "
               href={
                 "https://api.whatsapp.com/send?phone=5577981140394&text=Quero%20fazer%20um%20orçamento%20hoje!"
               }
             >
-              Whatsapp - (77) 98114-0394
+              Whatsapp - <span className="underline">(77) 98114-0394</span>
             </Link>
           </div>
         </section>
         <div className="mt-20 w-full flex flex-col  items-center justify-center ">
-          <p className="text-center text-sm text-gray-200 mt-5">
+          <p className="text-center text-sm max-sm:text-xs text-gray-200 mt-5">
             Ao navegar em nosso site você concorda com nosso Código de Ética e
             de Conduta.
           </p>
-          <p className="text-center text-sm text-gray-200 italic">
+          <p className="text-center text-sm max-sm:text-xs text-gray-200 italic">
             © 2024 • WSL Logística | Site feito com ❤ por{" "}
             <a
               href="https://ruyter.cloud"
